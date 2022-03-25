14f89c510ab8a77e5fe7f5ab3d125fc8019dcf67
迷糊虫 <kingxjs@163.com>
2022-03-25 11:03:23 +0800

    Update jd_dpqd.js

--- jd_dpqd.js
+++ jd_dpqd.js
@@ -202,7 +202,7 @@ function getActivityInfo(token, venderId) {
                     //console.log(data)
                     data = JSON.parse(/{(.*)}/g.exec(data)[0])
                     activityId = data.data.id
-                    console.log(JSON.stringify(data))
+                    // console.log(JSON.stringify(data))
                     let mes = '';
                     var levels = [];
                     for (let i = 0; i < data.data.continuePrizeRuleList.length; i++) {
