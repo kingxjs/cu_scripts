9dad914c2232cf8032f6f943ba1ffce2a0ad75d9
迷糊虫 <kingxjs@163.com>
2022-04-12 22:27:19 +0800

    Update jd_connoisseur.js

--- jd_connoisseur.js
+++ jd_connoisseur.js
@@ -151,7 +151,7 @@ async function getActiveInfo(url = 'https://prodev.m.jd.com/mall/active/2y1S9xVY
                 agid.push(vo.materialParams.advIdKOC[0].advGrpId)
                 console.log(`去做【${vo.boardParams.titleText}】`)
                 await getTaskInfo("10", vo.boardParams.projectCode, vo.boardParams.taskCode)
-              } else if (vo.boardParams && vo.boardParams.taskCode === "2PbAu1BAT79RxrM5V7c2VAPUQDSd") {
+              } else if (vo.boardParams && vo.boardParams.taskCode === "3DAwXrfC88jgHMvGpYzF1nBrUFgA") {
                 agid = []
                 agid.push(vo.materialParams.advIdKOC[0].advGrpId)
                 agid.push(vo.materialParams.advIdVideo[0].advGrpId)
@@ -162,7 +162,7 @@ async function getActiveInfo(url = 'https://prodev.m.jd.com/mall/active/2y1S9xVY
                 console.log(`去做【${vo.boardParams.titleText}】`)
                 await getTaskInfo("9", vo.boardParams.projectCode, vo.boardParams.taskCode)
                 await $.wait(2000)
-              } else if (vo.boardParams && (vo.boardParams.taskCode === "3dw9N5yB18RaN9T1p5dKHLrWrsX" || vo.boardParams.taskCode === "CtXTxzkh4ExFCrGf8si3ePxGnPy" || vo.boardParams.taskCode === "Hys8nCmAaqKmv1G3Y3a5LJEk36Y" || vo.boardParams.taskCode === "2wPBGptSUXNs3fxqgAtrV5MwkYEa")) {
+              } else if (vo.boardParams && (vo.boardParams.taskCode === "gmYjr6WER26XE8DmENe4BvbVNdm" || vo.boardParams.taskCode === "2M8sBL2RsSaE8nBrGhZGHrNcwNtA" || vo.boardParams.taskCode === "31MsESed1HY5c34gX2dnxD3C7YJF" || vo.boardParams.taskCode === "3FTPWUxG3B3tmFcmZ9e7WggNf2H5")  || vo.boardParams.taskCode === "284dYeG7rdPieV6mB6ZLpvRsqaXt")) {
                 await getTaskInfo("1", vo.boardParams.projectCode, vo.boardParams.taskCode)
                 await $.wait(2000)
               }
