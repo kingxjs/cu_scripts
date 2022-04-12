8f5b8faadf88dc45e912e9e760a13ac4c88eaf71
迷糊虫 <kingxjs@163.com>
2022-04-12 22:00:27 +0800

    Update jd_connoisseur.js

--- jd_connoisseur.js
+++ jd_connoisseur.js
@@ -137,7 +137,8 @@ async function getActiveInfo(url = 'https://prodev.m.jd.com/mall/active/2y1S9xVY
             encodeActivityId = data.activityInfo.encodeActivityId
             paginationFlrs = data.paginationFlrs
             activityId = data.activityInfo.activityId
-            for (var i = 0;i＜data.codeFloors.length;i++) {
+            for (let i = 0; i < data.codeFloors.length; i++) {
+            
               let vo = data.codeFloors[i]
               if (vo.boardParams && vo.boardParams.taskCode === "2CCbSBbVWkFZzRDngs4F6q3YZ62o") {
                 agid = []
