83a1931e8775b0ad0aedf62059338d1e69c6544e
迷糊虫 <kingxjs@163.com>
2022-05-31 10:05:02 +0800

    Update jd_redEnvelope.js

--- jd_redEnvelope.js
+++ jd_redEnvelope.js
@@ -98,10 +98,10 @@ async function main() {
     // }
     await $.wait(2000);
   }
-//   if ($.index === 1 && !$.hotFlag) {
-//       await $.wait(2000)
-//       await mainInfo()
-//   }
+  if ($.index === 1 && !$.hotFlag) {
+      await $.wait(2000)
+      await mainInfo()
+  }
 }
 
 function mainInfo() {
