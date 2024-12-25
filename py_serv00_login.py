8af2b6b50f1ae934a6af6115b01b985f10de3a9d
迷糊虫 <kingxjs@163.com>
2024-12-25 16:40:41 +0800

    Update py_serv00_login.py

--- py_serv00_login.py
+++ py_serv00_login.py
@@ -1,112 +1,70 @@
-# -- coding: utf-8 --
-"""
-const $ = new Env("serv00 保号");
-serv00 保号
-[Script]
-cron "0 0 10 ? * MON/14" script-path=serv00_login.py,tag=serv00 保号
-"""
-
-import json
-import asyncio
-from pyppeteer import launch
-from datetime import datetime, timedelta
-import aiofiles
-import random
-import requests
 import os
+import paramiko
+import json
 from notify import send
 
-def format_to_iso(date):
-    return date.strftime('%Y-%m-%d %H:%M:%S')
 
-async def delay_time(ms):
-    await asyncio.sleep(ms / 1000)
+def ssh_connect(host, port, username, password, command):
+    """
+    自动通过SSH连接服务器并执行命令。
 
-# 全局浏览器实例
-browser = None
-
-# 消息
-message = ""
-
-async def login(username, password, panelnum):
-    global browser
-
-    page = None  # 确保 page 在任何情况下都被定义
-    serviceName = 'ct8' if 'ct8' in panelnum else 'serv00'
+    :param host: 服务器IP地址或域名
+    :param port: SSH端口号
+    :param username: SSH用户名
+    :param password: SSH密码
+    :param command: 要执行的命令
+    :return: 命令执行的输出
+    """
     try:
-        if not browser:
-            browser = await launch(headless=True, args=['--no-sandbox', '--disable-setuid-sandbox'])
+        # 创建SSH客户端
+        client = paramiko.SSHClient()
+        # 自动添加未知主机到known_hosts
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        # 连接服务器
+        client.connect(hostname=host, port=port,
+                       username=username, password=password)
+        print(f"成功连接到 {host}:{port}")
 
-        page = await browser.newPage()
-        url = f'https://panel{panelnum}/login/?next=/'
-        await page.goto(url)
+        # 执行命令
+        stdin, stdout, stderr = client.exec_command(command)
+        output = stdout.read().decode()
+        error = stderr.read().decode()
 
-        username_input = await page.querySelector('#id_username')
-        if username_input:
-            await page.evaluate('''(input) => input.value = ""''', username_input)
+        if output:
+            msg = "连接{}服务器成功，命令输出：{}".format(host, output)
 
-        await page.type('#id_username', username)
-        await page.type('#id_password', password)
+        if error:
+            msg = "连接{}服务器成功，命令错误：{}".format(host, error)
 
-        login_button = await page.querySelector('#submit')
-        if login_button:
-            await login_button.click()
-        else:
-            raise Exception('无法找到登录按钮')
-
-        await page.waitForNavigation()
-
-        is_logged_in = await page.evaluate('''() => {
-            const logoutButton = document.querySelector('a[href="/logout/"]');
-            return logoutButton !== null;
-        }''')
-
-        return is_logged_in
+        print(msg)
+        return msg
 
     except Exception as e:
-        print(f'{serviceName}账号 {username} 登录时出现错误: {e}')
-        return False
-
+        msg = "连接{}服务器失败：{}".format(host, e)
+        print(msg)
+        return msg
     finally:
-        if page:
-            await page.close()
-# 显式的浏览器关闭函数
-async def shutdown_browser():
-    global browser
-    if browser:
-        await browser.close()
-        browser = None
+        # 确保关闭连接
+        client.close()
+        print("{}连接已关闭".format(host))
 
-async def main():
-    global message
+
+if __name__ == "__main__":
     accounts = os.environ["serv00_AUTH"]
     if accounts:
         accounts = json.loads(accounts)
 
+    msgs = "Serv00保号\n"
     for account in accounts:
-        username = account['username']
-        password = account['password']
-        panelnum = account['panelnum']
-
-        serviceName = 'ct8' if 'ct8' in panelnum else 'serv00'
-        is_logged_in = await login(username, password, panelnum)
-
-        now_beijing = format_to_iso(datetime.utcnow() + timedelta(hours=8))
-        if is_logged_in:
-            message += f"✅*{serviceName}*账号 *{username}* 于北京时间 {now_beijing}登录面板成功！\n\n"
-            print(f"{serviceName}账号 {username} 于北京时间 {now_beijing}登录面板成功！")
-        else:
-            message += f"❌*{serviceName}*账号 *{username}* 于北京时间 {now_beijing}登录失败\n\n❗请检查*{username}*账号和密码是否正确。\n\n"
-            print(f"{serviceName}账号 {username} 登录失败，请检查{serviceName}账号和密码是否正确。")
-
-        delay = random.randint(1000, 8000)
-        await delay_time(delay)
-        
-    message += f"🔚脚本结束，如有异常点击下方按钮👇"
-    send("serv00 保号",message)
-    print(f'所有{serviceName}账号登录完成！')
-    # 退出时关闭浏览器
-    await shutdown_browser()
-
-if __name__ == '__main__':
-    asyncio.run(main())
+        username = account['username']  # SSH用户名
+        password = account['password']  # SSH密码
+        server_host = account['server_host']  # 服务器IP或域名
+        server_port = account['server_port']  # SSH端口号,默认都为22
+        command_to_execute = account.get(
+            'command_to_execute', 'ls -la')  # 需执行的命令，默认都为ls -la
+        command_to_execute = command_to_execute.split("\n")
+        # 遍历所有的服务器配置
+        msg = ssh_connect(server_host, server_port,
+                          username, password, command_to_execute)
+        msgs += msg + "\n"
+    send("Serv00保号信息", msgs)
