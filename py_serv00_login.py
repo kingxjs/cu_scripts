1454969ae21cbe6f06745ed6939c103150de3832
迷糊虫 <kingxjs@163.com>
2024-12-27 14:44:17 +0800

    Update py_serv00_login.py

--- py_serv00_login.py
+++ py_serv00_login.py
@@ -11,6 +11,42 @@ import json
 from notify import send
 
 
+def format_pm2_list(output):
+    # 将输出按行拆分
+    lines = output.splitlines()
+
+    # 查找标题行和数据行
+    header = lines[1]  # 标题行在第2行（第一行是pm2 list的命令输出说明）
+
+    # 处理每一行输出，跳过前两行（表头和命令说明）
+    formatted_msg = ""
+    for line in lines[3:]:
+        # 分割每一行的列数据
+        columns = [column.strip() for column in line.split("│")]
+
+        # 去除空字符串
+        columns = [col for col in columns if col]
+
+        # 确保每行至少有12列
+        if len(columns) >= 12:
+            id = columns[0].strip()
+            name = columns[1].strip()
+            status = columns[8].strip()
+            pid = columns[5].strip()
+            uptime = columns[6].strip()
+            memory = columns[10].strip()
+            cpu = columns[9].strip()
+            user = columns[11].strip()
+
+            # 合并属性并简化输出
+            formatted_msg += f"{id}: {name} ({status})\n"
+            formatted_msg += f"PID: {pid} ({uptime})\n"
+            formatted_msg += f"Memory: {memory}\n"
+            formatted_msg += f"CPU: {cpu}\n"
+            formatted_msg += "------\n"
+
+    return formatted_msg
+
 def ssh_connect(host, port, username, password, command):
     try:
         # 创建SSH客户端
@@ -18,7 +54,7 @@ def ssh_connect(host, port, username, password, command):
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         client.connect(hostname=host, port=port, username=username, password=password)
         print(f"成功连接到 {host}:{port}")
-
+        ispm2 = command and 'pm2 list' in command[-1]
         # 确保传递的是字符串类型
         if isinstance(command, list):
             command = "\n".join(command)
@@ -26,12 +62,14 @@ def ssh_connect(host, port, username, password, command):
         stdin, stdout, stderr = client.exec_command(command)
         output = stdout.read().decode()
         error = stderr.read().decode()
-
+        msg = f"连接{host}服务器成功"
         if output:
-            msg = f"连接{host}服务器成功，命令输出：{output}"
-
+            if ispm2:
+                msg += f"\n\n命令输出：\n{format_pm2_list(output)}"
+            else:
+                msg += f"\n\n命令输出：\n{output}"
         if error:
-            msg = f"连接{host}服务器成功，命令错误：{error}"
+            msg += f"\n命令错误：{error}"
 
         print(msg)
         return msg
@@ -43,7 +81,6 @@ def ssh_connect(host, port, username, password, command):
     finally:
         client.close()
         print(f"{host}连接已关闭")
-
 
 
 if __name__ == "__main__":
